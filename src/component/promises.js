afe0f6621830f295edaaf993c349e7bba23e8db7
Solomon Gachure <gachuresolomon64@gmail.com>
2023-11-17 12:04:31 +0300

    Promises function worked successfully

--- src/component/promises.js
+++ src/component/promises.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
 const Promises = () => {
+
+    const p=new Promise ((resolve,reject)=>{
+        const a= 2+1
+        if (a==2){
+            resolve('Success')
+        } else{
+            reject('Failed')
+        }
+    })
+    p.then((message)=>{
+        console.log('This is true' + message)
+    })
+    .catch((message)=>{
+        console.log('This is false' + message)
+    })
   return (
-    <div>Promises</div>
+    <div>
+        
+    </div>
   )
 }
 
