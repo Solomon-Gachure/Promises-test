5dad0afd6d54cf7df3c6c8c9ca2c5470d39731cf
Solomon Gachure <gachuresolomon64@gmail.com>
2023-11-18 09:19:27 +0300

    Added the async function with axios as well, worked successfully

--- src/component/async.js
+++ src/component/async.js
@@ -1,8 +1,26 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 const Async = () => {
+    const [fact,setFact]=useState([]);
+const fetchdata=async ()=>{
+    try{
+        const response=await axios.get('https://catfact.ninja/fact')
+        setFact(response.data.fact);
+    }
+    catch (error){
+        console.log(error);
+    }
+};
+useEffect(()=>{
+    fetchdata();
+}, [])
+
+
+
   return (
-    <div>Async</div>
+    <div>
+        {fact}
+    </div>
   )
 }
 
