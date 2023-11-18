dfd497aadf080a42210a2bae575a6c8eef8620d7
Solomon Gachure <gachuresolomon64@gmail.com>
2023-11-18 09:56:32 +0300

    Successful fetch of API and use of async/await method

--- src/component/jokes.js
+++ src/component/jokes.js
@@ -1,8 +1,31 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 const Jokes = () => {
+
+const [jokes, setJokes]=useState([])
+const fetchdata=async ()=>{
+    try{
+        const response= await axios.get('https://official-joke-api.appspot.com/random_joke')
+        setJokes([response.data])
+    }
+    catch (error){
+        console.log('error', error)
+    }
+}
+useEffect(()=>{
+    fetchdata()
+},[])
+
   return (
-    <div>Jokes</div>
+    <div>
+        {jokes.map((joke)=>(
+            <div key={joke.id}>
+{joke.setup}
+{joke.punchline}
+            </div>
+        ))}
+        
+    </div>
   )
 }
 
